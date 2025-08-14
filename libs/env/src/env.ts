558285b0175d10e17ae5a1bf2e0306d99fe50696
Wagner Reis <wagnerreis1997@gmail.com>
2025-08-13 21:46:34 -0300

    feat: add env lib

---
+++ libs/env/src/env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  PORT: z.coerce.number().optional().default(3000),
+  NODE_ENV: z.string(),
+  DB_HOST: z.string(),
+  DB_PORT: z.string(),
+  DB_USER: z.string(),
+  DB_PASS: z.string(),
+  DB_NAME: z.string(),
+  // JWT_SECRET: z.string(),
+  // JWT_EXPIRATION: z.coerce
+  //   .number()
+  //   .optional()
+  //   .default(60 * 60 * 24),
+});
+
+export type Env = z.infer<typeof envSchema>;
