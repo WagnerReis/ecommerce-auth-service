54efef8fed03251901a2f059131e4611a955658f
Wagner Reis <wagnerreis1997@gmail.com>
2025-08-26 14:48:20 -0300

    fix(i18n): add eslint directive to suppress unused variable warning in FakeI18nService

--- test/i18n/fake-i18n.service.ts
+++ test/i18n/fake-i18n.service.ts
@@ -1,6 +1,7 @@
 import { I18nService } from 'nestjs-i18n';
 
 export class FakeI18nService implements Partial<I18nService> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   t(key: string, options?: any): any {
     // Mock simples que retorna a chave como valor
     // Você pode personalizar isso para retornar valores específicos se necessário
